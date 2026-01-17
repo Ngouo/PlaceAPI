deb3566c3f1ab61ceec7f75f1f97d45cb306f6bd
Gilbert Ngouo <149798152+Ngouo@users.noreply.github.com>
2026-01-17 14:42:24 +0100

    supabase

--- PlaceAPI/settings.py
+++ PlaceAPI/settings.py
@@ -92,9 +92,11 @@ USE_X_FORWARDED_HOST = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': 
-        dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
-    
+   'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # 'ENGINE': 'django.db.backends.postgresql_psycopg2',  Moteur de base de données
